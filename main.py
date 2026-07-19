6a77dd53bdcba59b8e25170893bc17d170ca8f3e
hongbo Wang <1040851739@qq.com>
2026-07-19 21:34:44 +0800

    获取基本信息

--- main.py
+++ main.py
@@ -4,25 +4,17 @@ myStock - A stock market analysis and tracking application.
 This module serves as the entry point for the myStock application,
 handling initialization and orchestration of the main program flow.
 """
-import configparser
-
-from sdk.basic.ifind_api import IFindAPI
-
-config = configparser.ConfigParser()
-config.read('config.ini', encoding='utf-8')
+from sdk.basic.tools import Tools
 
 def main():
     """
     This is the entry point for the myStock application.
     :return:
     """
-    refresh_token = config.get('DEFAULT', 'refresh_token')
-
-    iFind_api = IFindAPI(refresh_token=refresh_token) # pylint: disable=invalid-name
-    response = iFind_api.access_token
-    print(response)
-
-    print(config.get('DEFAULT', 'refresh_token'))
+    tools = Tools()
+    codes = tools.get_codes()
+    name = tools.get_ths_fund_inner_short_name_fund(codes)
+    print(f'code: {codes}, name: {name}')
 
 if __name__ == "__main__":
     main()
