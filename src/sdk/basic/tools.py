88a7772b922439868994cd8ac6b3b2391d8ed65b
hongbo Wang <1040851739@qq.com>
2026-07-19 21:53:40 +0800

    通过pylint检查

--- src/sdk/basic/tools.py
+++ src/sdk/basic/tools.py
@@ -1,3 +1,7 @@
+"""
+Utility Function Module
+Provide general-purpose utility functions, including features such as scheduled tasks and decorators.
+"""
 import configparser
 
 from sdk.basic.ifind_api import IFindAPI
@@ -14,16 +18,25 @@ class Tools:
         self.iFind_api = IFindAPI(self.refresh_token)  # pylint: disable=invalid-name
 
     def __get_refresh_token(self):
+        """
+        The function to obtain the iFind token
+        """
         refresh_token = self.config.get('DEFAULT', 'refresh_token')
 
         return refresh_token
 
     def get_codes(self):
+        """
+        The function to obtain the iFind stock codes
+        """
         codes = self.config.get('INFO', 'codes')
 
         return codes
 
     def get_ths_fund_inner_short_name_fund(self, codes):
+        """
+        The function to obtain the ths fund inner short name fund
+        """
         response = self.iFind_api.get_api_v1_basic_data_service(codes)
         ths_fund_inner_short_name_fund = response.json()['tables'][0]['table']['ths_fund_inner_short_name_fund'][0]
 
