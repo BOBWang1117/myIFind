926a741c6899dca5675cd00b34d0a7cd8a511dd1
hongbo Wang <1040851739@qq.com>
2026-07-19 21:33:35 +0800

    新增封装的调用函数

---
+++ sdk/basic/tools.py
@@ -0,0 +1,30 @@
+import configparser
+
+from sdk.basic.ifind_api import IFindAPI
+
+
+class Tools:
+    """
+    Encapsulates GET/POST request methods for the IFind data service.
+    """
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini', encoding='utf-8')
+        self.refresh_token = self.__get_refresh_token()
+        self.iFind_api = IFindAPI(self.refresh_token)  # pylint: disable=invalid-name
+
+    def __get_refresh_token(self):
+        refresh_token = self.config.get('DEFAULT', 'refresh_token')
+
+        return refresh_token
+
+    def get_codes(self):
+        codes = self.config.get('INFO', 'codes')
+
+        return codes
+
+    def get_ths_fund_inner_short_name_fund(self, codes):
+        response = self.iFind_api.get_api_v1_basic_data_service(codes)
+        ths_fund_inner_short_name_fund = response.json()['tables'][0]['table']['ths_fund_inner_short_name_fund'][0]
+
+        return ths_fund_inner_short_name_fund
